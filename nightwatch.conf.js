4329b38d13441fbba0ab1ec5a3e07196aa4e956d
sakshamarora-arch <sakshamarora@lambdatest.com>
2026-01-16 10:35:08 +0530

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,40 +1,26 @@
-module.exports = (function (settings) {
+module.exports = (function(settings) {
+  console.log(settings["test_settings"]["default"]["username"]);
 
-  console.log("Initial LT username:", settings.test_settings.default.username);
-
-  // Username from Jenkins
   if (process.env.LT_USERNAME) {
-    settings.test_settings.default.username = process.env.LT_USERNAME;
+    settings["test_settings"]["default"]["username"] = process.env.LT_USERNAME;
   }
 
-  // Access key from Jenkins
   if (process.env.LT_ACCESS_KEY) {
-    settings.test_settings.default.access_key = process.env.LT_ACCESS_KEY;
+    settings["test_settings"]["default"]["access_key"] = process.env.LT_ACCESS_KEY;
   }
 
-  // Selenium host override
   if (process.env.SELENIUM_HOST) {
     settings.selenium.host = process.env.SELENIUM_HOST;
   }
-
-  // Selenium port override
   if (process.env.SELENIUM_PORT) {
-    settings.selenium.port = Number(process.env.SELENIUM_PORT);
+    settings.selenium.host = process.env.SELENIUM_PORT;
   }
 
-  // Build name from Jenkins → LambdaTest
   if (process.env.LT_BUILD_NAME) {
-
-    // Ensure LT:Options exists
-    settings.test_settings.default.desiredCapabilities["LT:Options"] =
-      settings.test_settings.default.desiredCapabilities["LT:Options"] || {};
-
     settings.test_settings.default.desiredCapabilities["LT:Options"].build =
       process.env.LT_BUILD_NAME;
   }
 
-  console.log("Final Nightwatch settings:", JSON.stringify(settings, null, 2));
-
+  console.log(settings);
   return settings;
-
-})(require("./nightwatch.json"));
+})(require('./nightwatch.json'));
